a2f3d22006ffc98022a5855f59b9daeffb8500fd
Adrian Maslak <maslak.a92@gmail.com>
2024-10-17 14:49:54 +0200

    Vymena sprav

--- pks/Program.cs
+++ pks/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Konfigurácia portov a IP adresy
+        Console.Write("Zadajte svoj listen port: ");
+        int localPort = int.Parse(Console.ReadLine());
+
+        Console.Write("Zadajte remote IP adresu: ");
+        string remoteAddress = Console.ReadLine();
+
+        Console.Write("Zadajte remote port: ");
+        int remotePort = int.Parse(Console.ReadLine());
+
+        UdpPeer localPeer = new UdpPeer(localPort, remoteAddress, remotePort);
+        _ = localPeer.StartListeningAsync(message =>
+        {
+            Console.WriteLine($"Prijaté: {message}");
+        });
+
+        Console.WriteLine("Zadajte správy na odoslanie. Zadajte 'exit' pre ukončenie.");
+        while (true)
+        {
+            string messageToSend = Console.ReadLine();
+            if (messageToSend.ToLower() == "exit")
+                break;
+
+            await localPeer.SendMessageAsync(messageToSend);
+        }
+    }
+}
---
+++ pks/UdpPeer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+public class UdpPeer
+{
+    private UdpClient udpClient;
+    private IPEndPoint remoteEndPoint;
+    private int listenPort;
+
+    public UdpPeer(int listenPort, string remoteAddress, int remotePort)
+    {
+        this.listenPort = listenPort;
+        udpClient = new UdpClient(listenPort);
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(remoteAddress), remotePort);
+    }
+
+    public async Task StartListeningAsync(Action<string> onMessageReceived)
+    {
+        while (true)
+        {
+            var result = await udpClient.ReceiveAsync();
+            string receivedMessage = Encoding.UTF8.GetString(result.Buffer);
+            onMessageReceived?.Invoke(receivedMessage);
+        }
+    }
+
+    public async Task SendMessageAsync(string message)
+    {
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        await udpClient.SendAsync(messageBytes, messageBytes.Length, remoteEndPoint);
+    }
+}
+
+
